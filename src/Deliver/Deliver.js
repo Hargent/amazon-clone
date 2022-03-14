9fc2764093ddd25ff79f8fc1fa6ecdb1d8adc082
Hargent <94790646+Hargent@users.noreply.github.com>
2022-03-14 11:40:36 +0100

    auto-commit

--- src/Deliver/Deliver.js
+++ src/Deliver/Deliver.js
@@ -9,17 +9,21 @@ return(
 	<div className="delivery__container outer__container">
 		<div className="delivery__container inner">
 		<div className="delivery__container__title">
-		    <p >Choose your location</p> 
+			<p >Choose your location</p> 
 		</div>
 		<div className="delivery__container__content">
 			<h5>Delivery options and delivery speeds may vary for different locations</h5>
 			<h4>Manage address book</h4>
-			<h5 className="delivery__container__content__switch">or enter a US zip code</h5>
-			<div className="delivery__container__content__zip">
-				<input  className="delivery__container__content__input" />
-				<button className="delivery__container__content__button">Apply</button>
+			<div className="delivery__container__content__divider__container">
+				<h5 className="delivery__container__content__divider">or enter a US zip code</h5>
 			</div>
-			<h5 className="delivery__container__content__switch">or</h5>
+			<div className="delivery__container__content__zip__content">
+				<input onClick={()=>{}} className="delivery__container__zip__content__input" />
+				<button className="delivery__container__zip__content__button">Apply</button>
+			</div>
+			<div className="delivery__container__content__divider__container">
+				<h5 className="delivery__container__content__divider__two">or</h5>
+			</div>
 			<span class="a-button-text a-declarative" role="radiogroup" data-action="a-dropdown-button">
 				<span class="a-dropdown-prompt" id="GLUXCountryValue">Nigeria</span>
 			</span>
